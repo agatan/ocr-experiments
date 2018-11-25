c0c2fd222b9757ce1dbc4c6f1d5675ff343cf2c8
agatan <agatan039@gmail.com>
2018-11-25 23:31:25 +0900

    Use F.interpolate instead of nn.Upsampling

--- backbone.py
+++ backbone.py
@@ -1,4 +1,5 @@
 import torch.nn as nn
+import torch.nn.functional as F
 import torchvision.models
 
 
@@ -6,15 +7,10 @@ class ResNet50Backbone(nn.Module):
     def __init__(self, pretrained=False):
         super(ResNet50Backbone, self).__init__()
         self.resnet50 = torchvision.models.resnet50(pretrained=pretrained)
-        self.deconv1 = nn.Sequential(
-            nn.Conv2d(2048, 1024, kernel_size=1, stride=1, padding=0),
-            nn.UpsamplingBilinear2d(scale_factor=2))
-        self.deconv2 = nn.Sequential(
-            nn.Conv2d(1024, 512, kernel_size=1, stride=1, padding=0),
-            nn.UpsamplingBilinear2d(scale_factor=2))
-        self.deconv3 = nn.Sequential(
-            nn.Conv2d(512, 256, kernel_size=1, stride=1, padding=0),
-            nn.UpsamplingBilinear2d(scale_factor=2))
+        self.deconv1 = nn.Conv2d(
+            2048, 1024, kernel_size=1, stride=1, padding=0)
+        self.deconv2 = nn.Conv2d(1024, 512, kernel_size=1, stride=1, padding=0)
+        self.deconv3 = nn.Conv2d(512, 256, kernel_size=1, stride=1, padding=0)
 
     def forward(self, x):
         x = self.resnet50.relu(self.resnet50.bn1(self.resnet50.conv1(x)))
@@ -23,7 +19,10 @@ class ResNet50Backbone(nn.Module):
         x2 = self.resnet50.layer2(x1)
         x3 = self.resnet50.layer3(x2)
         x4 = self.resnet50.layer4(x3)
-        x = self.deconv1(x4) + x3
-        x = self.deconv2(x) + x2
-        x = self.deconv3(x) + x1
+        x = F.interpolate(self.deconv1(x4), scale_factor=2,
+                          mode='bilinear') + x3
+        x = F.interpolate(self.deconv2(x), scale_factor=2,
+                          mode='bilinear') + x2
+        x = F.interpolate(self.deconv3(x), scale_factor=2,
+                          mode='bilinear') + x1
         return x
