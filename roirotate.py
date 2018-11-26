2738d55b4dab9845c6f88b7090ec0b9503887b97
agatan <agatan039@gmail.com>
2018-11-26 14:43:57 +0900

    Refactor

--- roirotate.py
+++ roirotate.py
@@ -19,8 +19,8 @@ class RoIRotate(nn.Module):
         '''
         expand = False
         if len(image.size()) == 3:
-            image.unsqueeze_(0)
-            boxes.unsqueeze_(0)
+            image = image.unsqueeze(0)
+            boxes = boxes.unsqueeze(0)
             expand = True
         else:
             assert image.size(0) == boxes.size(0)
@@ -35,19 +35,26 @@ class RoIRotate(nn.Module):
             boxes = new_boxes
 
         batch_size = image.size(0)
-        batch_result_boxes = []
-        max_width = 0
-        max_boxes = 0
-        for img, bbs in zip(image, boxes):
-            base_widths = bbs[..., 2] - bbs[..., 0]
-            base_heights = bbs[..., 3] - bbs[..., 1]
-            c_img, h_img, w_img = img.size()
+        base_widths = boxes[..., 2] - boxes[..., 0]
+        base_heights = boxes[..., 3] - boxes[..., 1]
+        aspects = base_widths.type(torch.float32) / base_heights.type(torch.float32)
+        new_widths = aspects * self.height
+        new_widths[torch.isnan(new_widths)] = 0
+        new_widths = new_widths.type(torch.int32)
+        max_width = new_widths.max()
+        max_boxes = boxes.size()[1]
 
+        batch_result_boxes = []
+        for i in range(image.size()[0]):
             result_bbs = []
-            for box, base_width, base_height in zip(bbs, base_widths, base_heights):
-                aspect = base_width / base_height
-                width = int(aspect * self.height)
-                max_width = max(max_width, width)
+            img = image[i]
+            bbs = boxes[i]
+            c_img, h_img, w_img = img.size()
+            for j in range(bbs.size()[0]):
+                box = bbs[j]
+                base_width = base_widths[i, j]
+                base_height = base_heights[i, j]
+                width = new_widths[i, j]
                 each_w = base_width / (width - 1)
                 each_h = base_height / (self.height - 1)
                 xx = torch.arange(
@@ -59,12 +66,10 @@ class RoIRotate(nn.Module):
                 yy = yy.view(-1, 1).repeat(1, width).view(self.height, width)
                 result = bilinear_interpolate_torch(img, xx, yy)
                 result_bbs.append(result)
-
-            max_boxes = max(max_boxes, len(result_bbs))
             batch_result_boxes.append(result_bbs)
 
         result = torch.zeros(batch_size, max_boxes, c_img,
-                             self.height, max_width).requires_grad_(True)
+                             self.height, max_width)
         mask = torch.zeros(batch_size, max_boxes, max_width).byte()
         for i, boxes in enumerate(batch_result_boxes):
             for j, box in enumerate(boxes):
@@ -111,19 +116,21 @@ def main():
     import torchvision
     import torchvision.transforms as transforms
     img = Image.open("./out/0.png")
-    img = transforms.ToTensor()(img)
-    img = torch.autograd.Variable(img)
+    img = transforms.ToTensor()(img).requires_grad_(True)
     r = RoIRotate(100, vertical=True)
 
-    bbs = torch.Tensor([[0, 0, 100, 200], [100.1, 0, 300, 200]])
+    bbs = torch.Tensor([[0, 0, 100, 200], [100.1, 0, 300, 200]]).requires_grad_(True)
     boxes, masks = r.forward(img, bbs)
+    boxes.mean().backward()
+    print(bbs.grad, img.grad)
     torchvision.utils.save_image(boxes.data, 'a.jpg')
 
     img = Image.open("./out/1.png")
-    img = transforms.ToTensor()(img)
-    img = torch.autograd.Variable(img)
+    img = transforms.ToTensor()(img).requires_grad_(True)
     r = RoIRotate(100)
-    img = img.unsqueeze(0)
-    bbs = torch.Tensor([[[0, 0, 100, 200], [100, 0, 300, 200]]])
+    img = img.unsqueeze(0).requires_grad_(True)
+    bbs = torch.Tensor([[[0, 0, 100, 200], [100, 0, 300, 200]]]).requires_grad_(True)
     boxes, masks = r.forward(img, bbs)
+    boxes.mean().backward()
+    print(bbs.grad, img.grad)
     torchvision.utils.save_image(boxes.data[0], 'b.jpg')
