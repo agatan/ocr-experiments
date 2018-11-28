25d736e9d6b72045b9275dfca03923ddb502c2ae
agatan <agatan039@gmail.com>
2018-11-28 23:25:23 +0900

    Start implementing MobileNetV2Backbone

--- backbone.py
+++ backbone.py
@@ -26,3 +26,93 @@ class ResNet50Backbone(nn.Module):
         x = F.interpolate(self.deconv3(x), scale_factor=2,
                           mode='bilinear') + x1
         return x
+
+
+def _conv_bn(in_channels, out_channels, stride):
+    return nn.Sequential(
+        nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=stride, padding=1, bias=False),
+        nn.BatchNorm2d(out_channels),
+        nn.ReLU6(inplace=True),
+    )
+
+
+def _conv1x1_bn(in_channels, out_channels):
+    return nn.Sequential(
+        nn.Conv2d(in_channels, out_channels, kernel_size=1, bias=False),
+        nn.BatchNorm2d(out_channels),
+        nn.ReLU6(inplace=True),
+    )
+
+
+class _InvertedResidual(nn.Module):
+    def __init__(self, in_channels, out_channels, stride, expand_ratio):
+        super(_InvertedResidual, self).__init__()
+        self.use_residual = stride == 1 and in_channels == out_channels
+        if expand_ratio == 1:
+            self.conv = nn.Sequential(
+                # depthwise
+                nn.Conv2d(in_channels, in_channels, kernel_size=3, stride=stride, padding=1, bias=False, groups=in_channels),
+                nn.BatchNorm2d(in_channels),
+                nn.ReLU6(inplace=True),
+                # pointwise
+                nn.Conv2d(in_channels, out_channels, kernel_size=1, stride=1, bias=False),
+                nn.BatchNorm2d(out_channels),
+            )
+        else:
+            hidden_dim = round(in_channels * expand_ratio)
+            self.conv = nn.Sequential(
+                # pointwise
+                nn.Conv2d(in_channels, hidden_dim, kernel_size=1, stride=1, bias=False),
+                nn.BatchNorm2d(hidden_dim),
+                nn.ReLU6(inplace=True),
+                # depthwise
+                nn.Conv2d(hidden_dim, hidden_dim, kernel_size=3, stride=stride, padding=1, bias=False, groups=hidden_dim),
+                nn.BatchNorm2d(hidden_dim),
+                nn.ReLU6(inplace=True),
+                # pointwise
+                nn.Conv2d(hidden_dim, out_channels, kernel_size=1, stride=1, bias=False),
+                nn.BatchNorm2d(out_channels),
+            )
+
+    def forward(self, x):
+        if self.use_residual:
+            return x + self.conv(x)
+        return self.conv(x)
+
+
+class MobileNetV2Backbone(nn.Module):
+    def __init__(self):
+        super(MobileNetV2Backbone, self).__init__()
+        first_channels = 32
+        inverted_residual_config = [
+            # t (expand ratio), channels, n (layers), stride
+            [1, 16, 1, 1],
+            [6, 24, 2, 2],
+            [6, 32, 3, 2],
+            [6, 64, 4, 2],
+            [6, 96, 3, 1],
+            [6, 160, 3, 2],
+            [6, 320, 1, 1],
+        ]
+        self.conv1 = _conv_bn(3, first_channels, stride=2)
+        input_channels = first_channels
+        self.blocks = nn.ModuleList()
+        for i, (t, c, n, s) in enumerate(inverted_residual_config):
+            output_channels = c
+            layers = []
+            for j in range(n):
+                if j == 0:
+                    layers.append(_InvertedResidual(input_channels, output_channels, stride=s, expand_ratio=t))
+                else:
+                    layers.append(_InvertedResidual(input_channels, output_channels, stride=1, expand_ratio=t))
+                input_channels = output_channels
+            self.blocks.append(nn.Sequential(*layers))
+        last_channels = 1280
+        self.conv2 = _conv1x1_bn(input_channels, last_channels)
+        self.deconv1 = nn.Conv2d()
+
+    def forward(self, x):
+        x = self.conv1(x)
+        for block in self.blocks:
+            x = block(x)
+        return self.conv2(x)
